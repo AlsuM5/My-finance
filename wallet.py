05ba487d53ece54829df1b1b568a1647fd23f518
adkal1 <kaliev46@gmail.com>
2022-09-28 23:05:17 +0600

    changed and run pre-commit

--- wallet.py
+++ wallet.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-class Wallet:
 
+
+class Wallet:
     def __init__(self, days_count_to_end=30, initial_balance=0):
         self.expenses = []
         self.balance = initial_balance
@@ -16,22 +17,22 @@ class Wallet:
         return self.balance
 
     def get_daily_allowance(self):
-      return self.balance / self.days_count_to_end
-      
-    def add_schedel_expens(self,scheduel_expens):
-      self.scheduel_expense.append(scheduel_expens)
+        return self.balance / self.days_count_to_end
+
+    def add_schedel_expens(self, scheduel_expens):
+        self.scheduel_expense.append(scheduel_expens)
 
     def is_on_trec(self):
-      pass
-    
-class Expens:
+        pass
 
+
+class Expens:
     def __init__(self, value):
         self.value = value
         self.date = datetime.utcnow()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     wallet = Wallet(initial_balance=10000)
     wallet.add_expens(Expens(100))
     wallet.add_schedel_expens(199)
