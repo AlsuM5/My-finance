1774f3fda2e0036577403ef905e6a6bf6be2c800
Stasia Soloduha <teoretic.nest@gmail.com>
2022-10-01 10:57:56 +0300

    Fix tests.

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -3,6 +3,6 @@ from wallet import Wallet, Expens
 
 def test_wallet():
     wallet = Wallet(initial_balance=10000)
-    wallet.add_expense(Expens(100))
-    wallet.add_schedel_expense(199)
+    wallet.add_expenses(Expens(100))
+    wallet.add_schedule_expenses(199)
     assert wallet.get_daily_allowance() == 330
--- wallet.py
+++ wallet.py
@@ -7,7 +7,7 @@ class Wallet:
         self.balance = initial_balance
         self.reserved_balance = 0
         self.days_count_to_end = days_count_to_end
-        self.scheduel_expense = []
+        self.schedule_expenses = []
 
     def add_expenses(self, expens):
         self.expenses.append(expens)
@@ -19,8 +19,8 @@ class Wallet:
     def get_daily_allowance(self):
         return self.balance / self.days_count_to_end
 
-    def add_schedel_expenses(self, scheduel_expens):
-        self.scheduel_expenses.append(scheduel_expens)
+    def add_schedule_expenses(self, schedule_expens):
+        self.schedule_expenses.append(schedule_expens)
 
     def is_on_trec(self):
         pass
