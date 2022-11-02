870e98c8589136393e949c8d9534e610c497a1b6
Alsu Medvedeva <112749426+AlsuM5@users.noreply.github.com>
2022-11-02 08:36:02 +0300

    Update forms.py

--- webapp/forms.py
+++ webapp/forms.py
@@ -7,3 +7,4 @@ class LoginForm(FlaskForm):
     username = StringField('Имя пользователя',validators=[DataRequired()], render_kw={"class": "form-control"})
     password = PasswordField('Пароль',validators=[DataRequired()], render_kw={"class": "form-control"})
     submit = SubmitField('Отправить', render_kw={"class":"btn btn-primary"})
+    
