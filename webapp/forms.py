1df14e248f815dece2ba6eeb488fcbc90a2d5647
Alsu Medvedeva <alsuschka2@mail.com>
2022-11-03 16:05:08 +0300

    add_registration

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -13,5 +13,29 @@ class LoginForm(FlaskForm):
         "Пароль",
         validators=[DataRequired()],
         render_kw={"class": "form-control"},
+    )
+    submit = SubmitField("Отправить", render_kw={"class": "btn btn-primary"})
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        "Имя пользователя",
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+    )
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()],
+        render_kw={"class": "form-control"}
+    )
+    password = PasswordField(
+        "Пароль",
+        validators=[DataRequired()],
+        render_kw={"class": "form-control"}
+    )
+    password2 = PasswordField(
+        "Повторите пароль",
+        validators=[DataRequired(), EqualTo('password')],
+        render_kw={"class": "form-control"}
     )
     submit = SubmitField("Отправить", render_kw={"class": "btn btn-primary"})
