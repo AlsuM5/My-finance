e3ef37036514b6d9ff71cf851970f18a8fbd9cdf
adkal1 <kaliev46@gmail.com>
2022-11-05 15:15:36 +0600

    AddTransaction 2

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template  # redirect
+from flask import Flask, render_template, redirect, request
 
 from webapp.wallet import Wallet
-from webapp.forms import LoginForm, RegistrationForm  # AddTransactionForm
-
-# import model
+from webapp.forms import LoginForm, RegistrationForm, AddTransactionForm
+import model
 
 
 def create_app():
@@ -45,18 +44,18 @@ def create_app():
             "page/index.html", page_title=page_title, wallet=wallet
         )
 
-    # @app.route("wallets/<wallet_id>/add_transaction")
-    # def add_transaction(wallet_id, methods=["GET", "POST"]):
-    #     page_title = "Add transaction"
-    #     transaction_form = AddTransactionForm()
-    #     if request.method == "POST":
-    #         wallet = model.load_wallet(wallet_id)
-    #         wallet_db = model.dump_wallet(wallet)
-    #         return redirect("/")
-    #     return render_template(
-    #         "page/add_transaction.html",
-    #         page_title=page_title,
-    #         form=transaction_form,
-    #     )
+    @app.route("wallets/<wallet_id>/add_transaction", methods=["GET", "POST"])
+    def add_transaction(wallet_id):
+        page_title = "Add transaction"
+        transaction_form = AddTransactionForm()
+        if request.method == "POST":
+            wallet = model.load_wallet(wallet_id)
+            model.dump_wallet(wallet)
+            return redirect("/")
+        return render_template(
+            "page/add_transaction.html",
+            page_title=page_title,
+            form=transaction_form,
+        )
 
     return app
